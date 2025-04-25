408b1382964d421a6ee9552f998944a0d9d231fa
Ebenebe Chidera <ebenebechidera3@gmail.com>
2025-04-25 22:46:44 +0100

    stuff

--- src/app/api/bgremove/upload/route.ts
+++ src/app/api/bgremove/upload/route.ts
@@ -14,24 +14,51 @@ export async function POST(req: Request) {
   const session = await auth();
 
   if (!session) {
-    return NextResponse.json({ message: 'Sign In son' }, { status: 400 });
+    return NextResponse.json({ message: 'Sign In first' }, { status: 400 });
   }
+
   try {
     const formData = await req.formData();
-    const file = formData.get('file') as File;
+    const file = formData.get('file');
     const title = formData.get('title') as string;
 
-    if (!file) {
+    // Check if file exists and is an instance of File
+    if (!file || !(file instanceof File)) {
       return NextResponse.json(
-        { message: 'Please Provide File' },
+        { message: 'Please Provide a valid File' },
         { status: 400 }
       );
     }
 
+    // Check if file type is valid
+    if (!file.type || !file.type.startsWith('image/')) {
+      return NextResponse.json(
+        { message: 'Invalid file type, only image files allowed' },
+        { status: 400 }
+      );
+    }
+
+    if (!title) {
+      return NextResponse.json(
+        { message: 'Please Provide a Title' },
+        { status: 400 }
+      );
+    }
+
+    // Convert file to base64
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     const base64Image = `data:${file.type};base64,${buffer.toString('base64')}`;
 
+    // Check if base64Image is generated correctly
+    if (!base64Image || base64Image.trim().length === 0) {
+      return NextResponse.json(
+        { message: 'Error converting file to base64' },
+        { status: 500 }
+      );
+    }
+
+    // Upload image to Cloudinary
     const result = await cloudinary.v2.uploader.upload(base64Image, {
       folder: 'uploads',
       transformation: [
@@ -42,10 +69,12 @@ export async function POST(req: Request) {
       ],
     });
 
+    // Connect to the database
     await connectDB().catch((e) => {
       console.log(e);
     });
 
+    // Store the image information in the database
     const storedImage = new imageModel({
       imageUrl: result.secure_url,
       uploadedBy: session.user?.name,
@@ -55,6 +84,7 @@ export async function POST(req: Request) {
       name: session.user?.name,
     });
 
+    // Update the user's image list in the database
     await users.findOneAndUpdate(
       { email: session.user?.email }, // Find the user by email
       {
@@ -71,10 +101,11 @@ export async function POST(req: Request) {
       { new: true }
     );
 
+    // Save the image record
     await storedImage.save();
 
     return NextResponse.json(
-      { message: 'Upload Successful', url: result },
+      { message: 'Upload Successful', url: result.secure_url },
       { status: 200 }
     );
   } catch (error) {
