fd0764e984f0506841cf2ad9b451f0c678af18a0
Ebenebe Chidera <ebenebechidera3@gmail.com>
2025-04-25 01:23:11 +0100

    changed runtime

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,7 @@
 import Google from 'next-auth/providers/google';
 import type { NextAuthConfig } from 'next-auth';
-import connectDB from './db/mongodb';
-import { users } from './db/schema';
+// import connectDB from './db/mongodb';
+// import { users } from './db/schema';
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
@@ -11,36 +11,7 @@ export default {
       clientSecret: process.env.NEXT_PUBLIC_AUTH_GOOGLE_SECRET,
     }),
   ],
-  callbacks: {
-    async signIn({ user }) {
-      await connectDB();
-      const existingUser = await users.findOne({ email: user.email });
-      if (!existingUser) {
-        await users.create({
-          name: user.name,
-          email: user.email,
-          profile: user.image,
-          image: [],
-        });
-      }
-      return true;
-    },
-    // async session({ session, token }) {
-    //   if (token && session.user && token.sub) {
-    //     session.user.id = token.sub;
-    //   }
-    //   return session;
-    // },
-    // async jwt({ token }) {
-    //   return token;
-    // },
-    // async redirect({ url, baseUrl }) {
-    //   // If the url is within our own site
-    //   if (url.startsWith(baseUrl)) return url;
-    //   // Otherwise, fallback
-    //   return baseUrl + '/b';
-    // },
-  },
+
   pages: {
     signIn: '/',
   },
--- src/auth.ts
+++ src/auth.ts
@@ -4,12 +4,44 @@ import { MongoDBAdapter } from '@auth/mongodb-adapter';
 
 import authConfig from '@/auth.config';
 import clientPromise from '@/db/client';
+import connectDB from './db/mongodb';
+import { users } from './db/schema';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   ...authConfig,
   session: {
     strategy: 'database',
+  },
+  callbacks: {
+    async signIn({ user }) {
+      await connectDB();
+      const existingUser = await users.findOne({ email: user.email });
+      if (!existingUser) {
+        await users.create({
+          name: user.name,
+          email: user.email,
+          profile: user.image,
+          image: [],
+        });
+      }
+      return true;
+    },
+    // async session({ session, token }) {
+    //   if (token && session.user && token.sub) {
+    //     session.user.id = token.sub;
+    //   }
+    //   return session;
+    // },
+    // async jwt({ token }) {
+    //   return token;
+    // },
+    // async redirect({ url, baseUrl }) {
+    //   // If the url is within our own site
+    //   if (url.startsWith(baseUrl)) return url;
+    //   // Otherwise, fallback
+    //   return baseUrl + '/b';
+    // },
   },
   secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
 });
